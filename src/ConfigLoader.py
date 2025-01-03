cb61b6d3c1b3ea6673c161d19a5be04c799e2757
thphuc2703 <thphucnguyen2703@gmail.com>
2025-01-03 16:32:47 +0700

    change file config

--- src/ConfigLoader.py
+++ src/ConfigLoader.py
@@ -28,7 +28,7 @@ class PathParser:
         self.vocab = os.path.join(self.res, config_path['vocab_tweet'])
 
 config_fp = os.path.join(os.path.dirname(__file__), 'config.yml')
-config = yaml.load(open(config_fp, 'r'))
+config = yaml.load(open(config_fp, 'r'), Loader=yaml.FullLoader)
 config_model = config['model']
 
 dates = config['dates']
